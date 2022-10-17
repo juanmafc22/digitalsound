afcce849f7fa1f8626e097afdb373a38be2b9f2e
Juan Manuel Fernández <juanmafc22@gmail.com>
2022-10-17 07:44:23 -0300

    Commit pre push a Github. Detalle de producto version desktop sin finalizar

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -2,7 +2,16 @@ const productsController = {
 
     guitarrasYbajos: (req, res) => {
         res.render("guitarras-y-bajos")
-    }
+    },
+
+    item: (req, res) => {
+
+        const id = req.params.id;
+
+        res.render("guitarras-y-bajos-23")
+    },
+
+
 
 };
 
--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const productController = require("../controllers/product.controller");
 
-router.get("/guitarras-y-bajos", productController.guitarrasYbajos)
+router.get("/guitarras-y-bajos", productController.guitarrasYbajos);
+
+router.get("/guitarras-y-bajos/:id", productController.item);
 
 module.exports = router;
