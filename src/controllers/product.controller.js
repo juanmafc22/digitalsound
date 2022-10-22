008da08a8376b659c3bbfd1c2342089a09674230
Juan Manuel Fernández <juanmafc22@gmail.com>
2022-10-22 10:57:48 -0300

    Rutas al header y footer.ejs corregidas

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -2,18 +2,18 @@ const productsController = {
 
     // Response de la sección Guitarras y Bajos
     guitarrasYbajos: (req, res) => {
-        res.render("guitarras-y-bajos")
+        res.render("products/guitarras-y-bajos")
     },
 
     // Responde para el producto/item que viene por ruta parametrizada con req.params ID
     item: (req, res) => {
         const id = req.params.id;
-        res.render("guitarras-y-bajos-23")
+        res.render("products/guitarras-y-bajos-23")
     },
     
     // Respose para el carrito de compras 
     carrito: (req, res) => {
-        res.render("carrito.ejs")
+        res.render("products/carrito.ejs")
     }
 
 };
