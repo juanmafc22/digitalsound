142d12cab2dbf7149458e88ea7f76570ab3f91e6
Ramon Carafi <ramon.carafi@hotmail.com>
2022-11-18 17:45:07 -0300

    branch de creación de usuarios sin terminar

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,39 @@
+const fs = require("fs");
+const path = require("path");
+
+const usuariosFilePath = path.join(__dirname, "../data/usuarios.json");
+const usuarios = JSON.parse(fs.readFileSync(usuariosFilePath, "utf-8"));
+
+
+
 const usersController = {
+    // peticion get para acceder al formulario de registro??
     registro: (req, res) => {
         res.render("users/registro")
+    },
+
+    // POST para crear un usuario nuevo
+
+    creacionUsuario: (req, res) => {
+
+        let newUsuario = {
+            "id": Date.now(),
+            "nombre": req.body.nombre,
+            "apellido": req.body.apellido,
+            "email": req.body.email,
+            //"categoria": "usuario",
+            "password": req.body.contraseña,
+            //"imagen": "img-usuarios-1666987149191.png" 
+
+           
+        };
+
+        usuarios.push(newUsuario);
+
+        fs.writeFileSync(usuariosFilePath, JSON.stringify(usuario, null, " "));
+        
+        res.redirect("/login");
+
     },
 
     login: (req, res) => {
--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const usersController = require("../controllers/user.controller");
 const router = express.Router(); 
 
 const userController = require("../controllers/user.controller");
@@ -6,6 +7,8 @@ const userController = require("../controllers/user.controller");
 router.get("/login", userController.login);
 
 router.get("/registro", userController.registro);
+
+router.post("/registro", usersController.creacionUsuario);
 
 module.exports = router;
 
